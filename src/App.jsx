1e1d744243ac3cba970b20c83dddf466c63e135d
GABO <gumbaridzegabrieli@gmail.com>
2025-11-15 13:07:00 +0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use, useRef } from "react";
 import "./app.css";
 
 export default function App() {
@@ -16,14 +16,26 @@ useEffect(() => {
   };
 }, []);
 
+const nameref = useRef();
+const surnameref = useRef();
+
+useEffect(() => {
+    const element = surnameref.current;
+    element.focus();
+}, []);
+
 
   return (
     <div className="container">
       <button onClick={() => setShow(!show)}>
         {show ? "Hide Hello World" : "Show Hello World"}
       </button>
-
       {show && <h1 className="title">Hello World</h1>}
+
+
+      <input type="text" placeholder="enter your name..." ref={nameref} />
+      <input type="text" placeholder="enter your surname..." ref={surnameref} />
+      {/* <input type="text" placeholder="enter your surname..." autoFocus /> */}
     </div>
   );
 }
